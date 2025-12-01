a86cc33af2957bbe4ed4c324924bca7f7002f22e
Vinayak Arora <vinayakarora7461@gmail.com>
2025-12-01 23:40:58 +0530

    07: ai providers

--- src/trpc/routers/_app.ts
+++ src/trpc/routers/_app.ts
@@ -1,7 +1,16 @@
 import { inngest } from "@/inngest/client";
 import { createTRPCRouter, protectedProcedure } from "../init";
 import prisma from "@/lib/db";
+
+
 export const appRouter = createTRPCRouter({
+  testAi: protectedProcedure.mutation(async()=>{
+    
+    await inngest.send({
+      name: "execute/ai",
+    })
+    return {success:true, message:"AI job queued"};
+  }),
   getWorkflows: protectedProcedure.query(({ ctx }) => {
     return prisma.workflow.findMany();
   }),
