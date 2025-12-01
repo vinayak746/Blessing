68274a32d153cb65fa50c58ba49dce1589f56908
Vinayak Arora <vinayakarora7461@gmail.com>
2025-12-01 14:18:07 +0530

    06: background jobs

--- src/trpc/routers/_app.ts
+++ src/trpc/routers/_app.ts
@@ -1,12 +1,19 @@
-import { baseProcedure, createTRPCRouter, protectedProcedure } from "../init";
+import { inngest } from "@/inngest/client";
+import { createTRPCRouter, protectedProcedure } from "../init";
 import prisma from "@/lib/db";
 export const appRouter = createTRPCRouter({
-  getUsers: protectedProcedure.query(({ ctx }) => {
-    return prisma.user.findMany({
-      where: {
-        id: ctx.auth.user.id,
+  getWorkflows: protectedProcedure.query(({ ctx }) => {
+    return prisma.workflow.findMany();
+  }),
+  createWorkflow: protectedProcedure.mutation(async()=>{
+    await inngest.send({
+      name:"test/hello.world",
+      data:{
+        email:"vinayakarora7461@gmail.com"
       },
-    });
+
+    })
+    return {success:true, message:"job queued"};
   }),
 });
 // export type definition of API
