50e819835f04750ea0453dd7bf2e43d91db370fc
Vinayak Arora <vinayakarora7461@gmail.com>
2025-12-05 17:26:05 +0530

    12: workflows pagination

--- src/app/(dashboard)/(rest)/workflows/page.tsx
+++ src/app/(dashboard)/(rest)/workflows/page.tsx
@@ -4,11 +4,18 @@ import { HydrateClient } from "@/trpc/server";
 import { Suspense } from "react";
 import {ErrorBoundary} from "react-error-boundary"
 import { WorkflowsContainer, WorkflowsList } from "@/features/workflows/components/workflows";
+import type { SearchParams } from "nuqs/server";
+import { workflowsParamsLoader } from "@/features/workflows/server/params-loader";
 
-const page = async() => {
+type Props = {
+  searchParams: Promise<SearchParams>;
+}
+
+const page = async({searchParams}:Props) => {
   await requireAuth();
 
-  prefetchWorkflows();
+  const params = await workflowsParamsLoader(searchParams);
+  prefetchWorkflows(params);
   return (
       <WorkflowsContainer>
         <HydrateClient>
