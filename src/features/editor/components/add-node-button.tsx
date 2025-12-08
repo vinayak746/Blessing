10eef1d42ca774f6d6a1903c6cdd0bd059169e61
Vinayak Arora <vinayakarora7461@gmail.com>
2025-12-08 14:25:19 +0530

    15: editor

---
+++ src/features/editor/components/add-node-button.tsx
@@ -0,0 +1,20 @@
+"use client";
+
+import { PlusIcon } from "lucide-react";
+import { memo } from "react";
+import { Button } from "@/components/ui/button";
+
+export const AddNodeButton = memo(()=>{
+    return(
+        <Button 
+            onClick={()=>{}}
+            size="icon"
+            variant="outline"
+            className="bg-background"
+        >
+            <PlusIcon className="size-4" />
+        </Button>
+    )
+})
+
+AddNodeButton.displayName = "AddNodeButton";
--- src/features/editor/components/editor.tsx
+++ src/features/editor/components/editor.tsx
@@ -1,18 +1,77 @@
 "use client";
 
 import { ErrorView, LoadingView } from "@/components/entity-components";
+import { useState, useCallback } from "react";
+import {
+  ReactFlow,
+  applyNodeChanges,
+  applyEdgeChanges,
+  addEdge,
+  type Node,
+  type Edge,
+  type NodeChange,
+  type EdgeChange,
+  type Connection,
+  Background,
+  Controls,
+  MiniMap,
+  Panel,
+} from "@xyflow/react";
 import { useSuspenseWorkflow } from "@/features/workflows/hooks/use-workflows";
 
-export const EditorLoading = () => {
-    return<LoadingView message="Loading editor..."/>
-}
+import "@xyflow/react/dist/style.css";
+import { nodeComponents } from "@/config/node-components";
+import { AddNodeButton } from "./add-node-button";
 
-export const EditorError = () =>{
-  return <ErrorView message="Error loading editor"/>
+export const EditorLoading = () => {
+  return <LoadingView message="Loading editor..." />;
 };
+
+export const EditorError = () => {
+  return <ErrorView message="Error loading editor" />;
+};
+
+
 
 export const Editor = ({ workflowId }: { workflowId: string }) => {
   const { data: workflow } = useSuspenseWorkflow(workflowId);
 
-  return <p>{JSON.stringify(workflow, null, 2)}</p>;
+  const [nodes, setNodes] = useState<Node[]>(workflow.nodes);
+  const [edges, setEdges] = useState<Edge[]>(workflow.edges);
+
+  const onNodesChange = useCallback(
+    (changes: NodeChange[]) =>
+      setNodes((nodesSnapshot) => applyNodeChanges(changes, nodesSnapshot)),
+    [],
+  );
+  const onEdgesChange = useCallback(
+    (changes: EdgeChange[]) =>
+      setEdges((edgesSnapshot) => applyEdgeChanges(changes, edgesSnapshot)),
+    [],
+  );
+  const onConnect = useCallback(
+    (params: Connection) => setEdges((edgesSnapshot) => addEdge(params, edgesSnapshot)),
+    [],
+  );
+  return (
+     <div className="size-full">
+      <ReactFlow
+        nodes={nodes}
+        edges={edges}
+        onNodesChange={onNodesChange}
+        onEdgesChange={onEdgesChange}
+        onConnect={onConnect}
+        nodeTypes={nodeComponents}
+        fitView
+        // proOptions={{ hideAttribution: true }} // Uncomment if u dont want to show ReactFlow attribution
+      >
+        <Background />
+        <Controls />
+        <MiniMap />
+        <Panel position="top-right">
+          <AddNodeButton />
+        </Panel>
+      </ReactFlow>
+    </div>
+  )
 };
