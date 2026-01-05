80180bff16d35d950191998c95ec6856ac64b188
Vinayak Arora <vinayakarora7461@gmail.com>
2026-01-05 19:26:35 +0530

    28: encrypting credentials

--- src/features/executions/components/anthropic/executor.ts
+++ src/features/executions/components/anthropic/executor.ts
@@ -5,6 +5,7 @@ import { createAnthropic } from "@ai-sdk/anthropic";
 import Handlebars from "handlebars";
 import { anthropicChannel } from "@/inngest/channels/anthropic";
 import prisma from "@/lib/db";
+import { decrypt } from "@/lib/encryption";
 
 Handlebars.registerHelper("json", (context) => {
   const jsonString = JSON.stringify(context, null, 2);
@@ -89,7 +90,7 @@ export const anthropicExecutor: NodeExecutor<AnthropicData> = async ({
   }
 
   const anthropic = createAnthropic({
-    apiKey: credential.value,
+    apiKey: decrypt(credential.value),
   });
 
   try {
--- src/features/executions/components/gemini/executor.ts
+++ src/features/executions/components/gemini/executor.ts
@@ -5,6 +5,7 @@ import { createGoogleGenerativeAI} from "@ai-sdk/google";
 import Handlebars from "handlebars";
 import { geminiChannel } from "@/inngest/channels/gemini";
 import prisma from "@/lib/db";
+import { decrypt } from "@/lib/encryption";
 
 Handlebars.registerHelper("json", (context) => {
   const jsonString = JSON.stringify(context, null, 2);
@@ -96,7 +97,7 @@ export const geminiExecutor: NodeExecutor<GeminiData> = async ({
   
 
   const google = createGoogleGenerativeAI({
-    apiKey: credential.value,
+    apiKey: decrypt(credential.value),
   });
 
   try{
